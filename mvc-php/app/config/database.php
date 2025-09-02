f6f24be3329c5b53605db848ec1f598c77431fb9
Victor Evangelista <elliottsmith_steven@hotmail.com>
2025-09-01 22:07:32 -0500

    Se creó login

--- mvc-php/app/config/database.php
+++ mvc-php/app/config/database.php
@@ -2,24 +2,25 @@
 class Database {
     private $host = "localhost";
     private $db_name = "SistemaVentas";
-    private $username = "root";
-    private $password = "";
-    public $conn;
+    private $username = "root";   
+    private $password = "sqladmin";       
+    private $conn;
 
     public function getConnection() {
         $this->conn = null;
 
         try {
-            $this->conn = new PDO("mysql:host=".$this->host.";dbname=".$this->db_name, 
-                                  $this->username, 
-                                  $this->password,
-                                  array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            $this->conn->exec("set names utf8mb4");
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch(PDOException $e) {
-            echo "Error de conexión: " . $e->getMessage();
+        } catch (PDOException $exception) {
+            echo "Error de conexión: " . $exception->getMessage();
         }
 
         return $this->conn;
     }
 }
-?>
